350e1165239084b4d4dbc6f6437652d1f5e70c6c
Blyatman0692 <jzheng2@seattleu.edu>
2026-01-19 15:07:31 -0800

    feat: improved vbml formatting methods

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict, Any
 
 
 def normalize_weather_results(results):
@@ -26,26 +26,30 @@ def format_weather_for_vestaboard(weather_list) -> str:
     # Vestaboard supports max ~6 lines
     return "\n".join(lines[:6])
 
-def compose_vbml_payload(message: str,
-                         height: int = 6,
-                         width: int = 22,
-                         justify: str = "left",
-                         align: str = "top"
-                         ):
+
+
+
+def compose_vbml_payload(components: List[dict]) -> Dict[str, Any]:
     return {
         "style": {
             "height": 6,
-            "width": 22
+            "width": 22,
         },
-        "components": [
-            {
-                "template": message,
-                "style": {
-                    "height": height,
-                    "width": width,
-                    "justify": justify,
-                    "align": align
-                }
-            }
-        ]
+        "components": components
+    }
+
+def compose_vbml_component(template: str,
+                           height: int = 1,
+                           width:int = 22,
+                           justify: str = "left",
+                           align: str = "top"
+                           ):
+    return {
+        "style": {
+            "height": height,
+            "width": width,
+            "justify": justify,
+            "align": align,
+        },
+        "template": template
     }
