b45a993abc9d2f8eb510e1b46d1b12863f0c9a76
Blyatman0692 <jzheng2@seattleu.edu>
2026-01-19 16:07:12 -0800

    feat: added a weather specific header using today's date, and a color index for each weather to be displayed

--- weather_app/run_weather.py
+++ weather_app/run_weather.py
@@ -5,6 +5,7 @@ import utils
 import weather
 from typing import List
 from dotenv import load_dotenv
+from datetime import date
 from vestaboard import VestaboardMessenger
 from weather_app.weather import WeatherNow
 
@@ -18,6 +19,10 @@ logging.basicConfig(
 
 logger = logging.getLogger()
 
+def get_headers():
+    pass
+
+
 def run():
     logger.info("Weather job started")
 
@@ -27,16 +32,19 @@ def run():
     vb = VestaboardMessenger()
     weather_data = List[WeatherNow]
 
+    header_string = 2 * "{66}" + 2 * "{68}" + date.today().strftime(
+        "%Y-%m-%d") + 2 * "{68}" + 2 * "{66}"
+
     try:
         weather_data = wc.get_current_weather_multi_cities()
         logger.info(f"Successfully retrieved weather info:\n{weather_data}")
     except Exception as e:
         logger.exception(f"Error retrieving weather info: {e}")
 
-    vbml_components = []
+    vbml_components = [utils.compose_vbml_component(header_string, justify="center", align="top")]
 
     for now in weather_data:
-        weather_string = weather.format_weather_line(now)
+        weather_string = "{67}" + weather.format_weather_line(now)
         component = utils.compose_vbml_component(weather_string)
         vbml_components.append(component)
 
