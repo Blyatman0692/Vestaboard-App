eee1c52e1b59349cf8d2e4c8c4defa5cd7394d6b
Blyatman0692 <jzheng2@seattleu.edu>
2026-01-22 22:22:50 -0800

    feat: script to compose countdown header, countdown contents and send to vestaboard to display

---
+++ countdown_app/run_countdown.py
@@ -0,0 +1,76 @@
+import logging
+import sys
+
+from typing import List
+from dotenv import load_dotenv
+
+from countdown_app.countdown import CountDown
+from vestaboard import VestaboardMessenger
+from targets import TARGET_DATES
+import utils
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout)
+    ],
+)
+
+logger = logging.getLogger()
+
+def run():
+    vb = VestaboardMessenger()
+    ct = CountDown(TARGET_DATES)
+    results = ct.calculate_date_delta()
+
+    vbml_components = []
+
+    vbml_components.append(
+        utils.compose_vbml_component(
+            "time until",
+            height=1,
+            width=22,
+            justify="left",
+            align="top"
+        )
+    )
+
+    vbml_components.append(
+        utils.compose_vbml_component(
+            "{63}{63}{63}{63}{63}{63}{63}{63}{63}{63}{63}{63}{63}{63}{63}{63}{63}{63}{63}{63}{63}{63}",
+            height=1,
+            width=22,
+            justify="left",
+            align="top"
+        )
+    )
+
+
+    for description, result in results.items():
+        description_component = utils.compose_vbml_component(
+            description,
+            height=2,
+            width=11,
+            justify="left",
+            align="top"
+        )
+        vbml_components.append(description_component)
+
+        timedelta_component = utils.compose_vbml_component(
+            str(CountDown.breakdown(result.delta)["days"]) + " days",
+            height=2,
+            width=11,
+            justify="right",
+            align="top"
+        )
+        vbml_components.append(timedelta_component)
+
+    vbml_payload = utils.compose_vbml_payload(vbml_components)
+    print(vbml_payload)
+    vbml_layout = vb.vbml_compose_layout(vbml_payload)
+    vb.send_layout(vbml_layout)
+
+if __name__ == "__main__":
+    run()
