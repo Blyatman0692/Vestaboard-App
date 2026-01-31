0cf10cf34f2635a581ef40ff972417de5751a06d
Blyatman0692 <jzheng2@seattleu.edu>
2026-01-31 11:14:35 -0800

    chore: updated countdown targets

--- countdown_app/targets.py
+++ countdown_app/targets.py
@@ -4,4 +4,6 @@ from datetime import datetime
 TARGET_DATES = {
     "Retirement": datetime(2029, 12, 7),
     "Pick up Staccato": datetime(2026, 2, 3),
+    "HW3: MPI": datetime(2026, 2, 15),
+    "Midterm 2": datetime(2026, 2, 19)
 }
