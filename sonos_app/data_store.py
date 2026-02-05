a81704cdbe5b46d14c75648351b001cebd9fb37f
Blyatman0692 <jzheng2@seattleu.edu>
2026-02-05 10:32:34 -0800

    feat: added a datastore client to handle saving and retrieving tokens generated

---
+++ sonos_app/data_store.py
@@ -0,0 +1,30 @@
+import psycopg
+
+class PostgresDataStore:
+    def __init__(self, db_url, user_key):
+        self.db_url = db_url
+        self.user_key = user_key
+
+    def save_tokens(self, tokens: dict[str, str]):
+        with psycopg.connect(self.db_url) as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    insert into sonos_tokens (user_key, access_token, refresh_token, expires_in, scope, updated_at)
+                    values (%s, %s, %s, %s, %s, now())
+                    on conflict (user_key) do update set
+                      access_token = excluded.access_token,
+                      refresh_token = excluded.refresh_token,
+                      expires_in = excluded.expires_in,
+                      scope = excluded.scope,
+                      updated_at = now()
+                    """,
+                    (
+                        self.user_key,
+                        tokens["access_token"],
+                        tokens["refresh_token"],
+                        tokens.get("expires_in"),
+                        tokens.get("scope")
+                    )
+                )
+                conn.commit()
