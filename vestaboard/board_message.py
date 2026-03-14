26d631814c8d8d0040938e7b397f9e8412c063ab
Blyatman0692 <jzheng2@seattleu.edu>
2026-03-13 18:36:19 -0700

    refactor: made layout in BoardMessage optional

--- vestaboard/board_message.py
+++ vestaboard/board_message.py
@@ -6,6 +6,6 @@ from vestaboard.board_state import BoardState
 @dataclass
 class BoardMessage:
     state: BoardState
-    layout: List[List[int]]
     source: str
+    layout: Optional[List[List[int]]] = None
     text: Optional[str] = None
